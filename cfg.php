4f9016a4747edf1734ac6742688bc47a9bc2efb7
Nsec25 <68063915+Nsec25@users.noreply.github.com>
2022-07-09 21:11:10 +0700

    Add files via upload

---
+++ cfg.php
@@ -0,0 +1,14 @@
+<?php
+
+$useragent = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.40 Safari/537.36";
+
+$cookie = "captcha=recaptchav2; popcashpu=1; _gid=GA1.2.360490203.1657374739; HstCfa4531111=1657374739746; HstCmu4531111=1657374739746; HstCnv4531111=1; HstCns4531111=1; _data_cpc=481-1-1657378338; _data_html=93-1; __dtsu=51A01657374740D3FB74A1EC4A6AA911; csrf_cookie_name=6f739baea68e6709031585c0c3cd49c2; ci_session=ef2fc5a72b8e5f12dbf017ef438c1dd289ed0aa4; _gat_gtag_UA_70132428_1=1; _ga_ERZS1914YB=GS1.1.1657374706.1.1.1657375689.0; HstCla4531111=1657375693424; HstPn4531111=14; HstPt4531111=14; _ga=GA1.2.923052151.1657374706; _data_pop=370-2-1657378337_425-1-1657378346_371-1-1657378358_476-3-1657378376_494-1-1657378988_304-2-1657379247_329-2-1657379259_473-1-1657379284";
+
+//your faucetpay email
+$fp = "nsec1209@gmail.com";
+
+//select coin auto wd (LTC:1/DOGE:2/FEY:3)
+$coin = "LTC:1";
+
+//autowd minimum 10
+$set = "11";
